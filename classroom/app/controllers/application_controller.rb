94c075bb6633b0f261d957d66f66bba2089fbc93
aguspina <aguspina87@gmail.com>
2016-01-13 18:23:32 -0300

    Add multitenat by subdomain

--- classroom/app/controllers/application_controller.rb
+++ classroom/app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  before_action :set_mongo_connection
+
+
+
+  private
+
+  def set_mongo_connection
+    tenant = request.subdomain.present?? request.subdomain : 'central'
+    client = Mongo::Client.new([ '127.0.0.1:27017' ], database: tenant)
+  end
+
 end
