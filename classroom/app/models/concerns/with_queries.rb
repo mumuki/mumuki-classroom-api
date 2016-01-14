baf293c4d238cc9e62315f22e575033381943f2a
Agustin Pina <aguspina87@gmail.com>
2016-01-14 16:35:41 -0300

    Add where query

--- classroom/app/models/concerns/with_queries.rb
+++ classroom/app/models/concerns/with_queries.rb
@@ -1,7 +1,11 @@
 module WithQueries
 
   def save! type, json, request
-    binding.pry
     request.env['mongo_client'][type].insert_one json
   end
+
+  def where type, json, request
+    request.env['mongo_client'][type].find json
+  end
+
 end
