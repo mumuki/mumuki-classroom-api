2c0c176aebe5c8fc2f7aa2aabaa2dd72a23db59b
Agustin Pina <aguspina87@gmail.com>
2016-01-24 23:47:33 -0300

    Remove unused code

--- config/initializers/active_record_belongs_to_required_by_default.rb
+++ config/initializers/active_record_belongs_to_required_by_default.rb
@@ -2,4 +2,3 @@
 
 # Require `belongs_to` associations by default. This is a new Rails 5.0 default,
 # so introduced as a config to ensure apps made with earlier versions of Rails aren't affected when upgrading.
-# Rails.application.config.active_record.belongs_to_required_by_default = true
