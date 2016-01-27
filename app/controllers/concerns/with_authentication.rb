fe49681059619e761b3816e7c9cb609aed3c4f6d
Agustin Pina <aguspina87@gmail.com>
2016-01-27 13:20:38 -0300

    Add exceptions catching for token errors

--- app/controllers/concerns/with_authentication.rb
+++ app/controllers/concerns/with_authentication.rb
@@ -1,13 +1,20 @@
 module WithAuthentication
 
   def permissions
-    token = Mumukit::Auth::Token.decode_header(authorization_header)
-    token.verify_client!
-    @permissions ||= token.permissions 'classroom'
+    @permissions ||= parse_token.permissions 'classroom'
   end
 
   def protect!
     permissions.protect! slug
+  rescue Mumukit::Auth::UnauthorizedAccessError => e
+    render json: { status: :unauthorized, message: e.message }, status: 403
+  rescue Mumukit::Auth::InvalidTokenError
+    render json: { status: :errored }, status: 400
+  end
+
+  def parse_token
+    token = Mumukit::Auth::Token.decode_header(authorization_header)
+    token.tap &:verify_client!
   end
 
   def authorization_header
