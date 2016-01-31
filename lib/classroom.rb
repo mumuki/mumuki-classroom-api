5804cc6d19a7962908a13b7bc969856191de6003
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-01-31 20:49:32 -0300

    Removing odd enters

--- lib/classroom.rb
+++ lib/classroom.rb
@@ -2,12 +2,8 @@ module Classroom
 end
 
 require 'active_support/all'
-
 require 'mongo'
-
 require_relative './classroom/with_mongo'
-
 require_relative './classroom/course'
-
 require_relative './classroom/guide_progress'
 
