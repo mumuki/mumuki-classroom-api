332cefdbd6e7828c9ecff295c1533cb0ea8cbb96
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-01-31 22:39:43 -0300

    Fixing method_missing bug

--- lib/classroom/with_mongo.rb
+++ lib/classroom/with_mongo.rb
@@ -1,7 +1,7 @@
 module Classroom::WithMongo
   def method_missing(name, *args, &block)
-    collection_name = name.to_s.split('_collection').first
-    if collection_name
+    if name.to_s.end_with? '_collection'
+      collection_name = name.to_s.split('_collection').first
       Classroom::Database.client[collection_name]
     else
       super
