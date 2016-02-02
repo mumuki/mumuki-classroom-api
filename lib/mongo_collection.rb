250884c6a6c52ee47154abcc2b751def5e6a8fbc
Agustin Pina <aguspina87@gmail.com>
2016-02-02 15:56:23 -0300

    Add course to mongo collection upsert method

--- lib/mongo_collection.rb
+++ lib/mongo_collection.rb
@@ -1,21 +1,21 @@
 class Mongo::Collection
   def upsert(json)
-    result = find({guide: json[:guide], student: json[:submitter]})
+    result = find({guide: json[:guide], student: json[:submitter], course: json[:course]})
 
     if result.count.zero?
-      insert_one({guide: json[:guide], student: json[:submitter],
+      insert_one({guide: json[:guide], student: json[:submitter], course: json[:course],
                   exercises: [{id: json[:exercise][:id], name: json[:exercise][:name], submissions: [json[:exercise][:submission]]}]})
 
     else
-      result2 = find({:guide => json[:guide], :student => json[:submitter], 'exercises.id' => json[:exercise][:id]})
+      result2 = find({:guide => json[:guide], :student => json[:submitter], course: json[:course], 'exercises.id' => json[:exercise][:id]})
 
       if result2.count.zero?
         update_one(
-            {guide: json[:guide], student: json[:submitter]},
+            {guide: json[:guide], student: json[:submitter], course: json[:course]},
             {'$push' => {'exercises' => {id: json[:exercise][:id], name: json[:exercise][:name], submissions: [json[:exercise][:submission]]}}})
       else
         update_one(
-            {guide: json[:guide], student: json[:submitter], 'exercises.id' => json[:exercise][:id]},
+            {guide: json[:guide], student: json[:submitter], course: json[:course], 'exercises.id' => json[:exercise][:id]},
             {'$push' => {'exercises.$.submissions' => json[:exercise][:submission]}})
       end
     end
@@ -34,6 +34,6 @@ class Mongo::Collection
   end
 
   def get_exercise(slug, student_id)
-    find('guide.slug' => slug, 'student.id' => student_id).first
+    find('guide.slug' => slug, 'student.social_id' => student_id).first
   end
 end
