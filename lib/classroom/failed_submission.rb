67f09d453c35fae5c7a6068fc75d074da6b613c5
Federico Scarpa <fedescarpa@gmail.com>
2016-02-11 14:17:30 -0300

    fixed connection close on reprocess

--- lib/classroom/failed_submission.rb
+++ lib/classroom/failed_submission.rb
@@ -1,21 +1,39 @@
 module Classroom::FailedSubmission
 
   def self.reprocess!(social_id, destination, logger=nil)
-    [destination, :central].each do |source|
-      Classroom::Database.organization = source
-      failed_submissions = Classroom::Collection::FailedSubmissions.find_by_social_id(social_id)
+    reprocess_current_organization social_id, destination, logger
+    reprocess_central social_id, destination, logger
+  end
 
-      failed_submissions.raw.each do |it|
-        Classroom::Database.organization = source
-        Classroom::Collection::FailedSubmissions.delete! it.id
-        begin
-          Classroom::Database.organization = destination
-          Classroom::Submission.process! it.raw
-        rescue => e
-          logger.warn "Resubmission failed #{e}. it was: #{it.raw}" if logger.present?
-          Classroom::Database.organization = source
-          Classroom::Collection::FailedSubmissions.insert! it
-        end
+  def self.reprocess_current_organization(social_id, destination, logger)
+    Classroom::Database.organization = destination
+    Classroom::Collection::FailedSubmissions.find_by_social_id(social_id).raw.each do |it|
+      Classroom::Collection::FailedSubmissions.delete! it.id
+      begin
+        Classroom::Submission.process! it.raw
+      rescue => e
+        logger.warn "Resubmission failed #{e}. it was: #{it.raw}" if logger.present?
+        Classroom::Collection::FailedSubmissions.insert! it
+      end
+    end
+    Classroom::Database.client.try(:close)
+  end
+
+  def self.reprocess_central(social_id, destination, logger)
+    Classroom::Database.organization = :central
+    Classroom::Collection::FailedSubmissions.find_by_social_id(social_id).raw.each do |it|
+      Classroom::Database.organization = :central
+      Classroom::Collection::FailedSubmissions.delete! it.id
+      Classroom::Database.client.try(:close)
+      begin
+        Classroom::Database.organization = destination
+        Classroom::Submission.process! it.raw
+        Classroom::Database.client.try(:close)
+      rescue => e
+        logger.warn "Resubmission failed #{e}. it was: #{it.raw}" if logger.present?
+        Classroom::Database.organization = :central
+        Classroom::Collection::FailedSubmissions.insert! it
+        Classroom::Database.client.try(:close)
       end
     end
   end
