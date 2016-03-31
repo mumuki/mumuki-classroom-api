16f46225eac420fab7434ddc2b96802423d9d2d8
Agustin Pina <aguspina87@gmail.com>
2016-03-31 12:33:52 -0300

    Change to exercise id

--- app/routes.rb
+++ app/routes.rb
@@ -166,9 +166,9 @@ post '/comment' do
   {status: :created}
 end
 
-get '/comments/:submission_id' do
+get '/comments/:exercise_id' do
   protect!
-  {comments: Classroom::Comment.where(submission_id: params[:submission_id].to_i)}
+  {comments: Classroom::Comment.where(exercise_id: params[:exercise_id].to_i)}
 end
 
 get '/ping' do
