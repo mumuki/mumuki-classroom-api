4a263ce95b031795dcbcab401435225a366b4465
Federico Scarpa <fedescarpa@gmail.com>
2016-04-28 11:48:32 -0300

    added default projection to query methods

--- lib/classroom/collection/comments.rb
+++ lib/classroom/collection/comments.rb
@@ -23,13 +23,13 @@ module Classroom::Collection::Comments
 end
 
 module Mumukit::Service::Collection
-  def where(args)
-    raw = mongo_collection.find(args).projection(_id: 0).map { |it| wrap it }
+  def where(args, projection={})
+    raw = mongo_collection.find(args).projection(projection.merge(_id: 0)).map { |it| wrap it }
     wrap_array raw
   end
 
-  def order_by(args, options)
-    mongo_collection.find(args).sort(options).projection(_id: 0).first.try{ |it| wrap(it) }
+  def order_by(args, options, projection={})
+    mongo_collection.find(args).sort(options).projection(projection.merge(_id: 0)).first.try{ |it| wrap(it) }
   end
 
 end
