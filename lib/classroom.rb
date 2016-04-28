00a8c3a8791577c491e3d69c88ab4c353c623e11
Federico Scarpa <fedescarpa@gmail.com>
2016-04-28 13:42:53 -0300

    removing WithMongo module

--- lib/classroom.rb
+++ lib/classroom.rb
@@ -4,7 +4,6 @@ end
 require 'mumukit/service'
 require 'active_support/all'
 
-require_relative './classroom/with_mongo'
 require_relative './classroom/database'
 require_relative './classroom/json_wrapper'
 require_relative './classroom/follower'
