677e2390e12f0b0bdf6e39e4c880654e45d20fe1
Federico Scarpa <fedescarpa@gmail.com>
2016-04-28 22:16:53 -0300

    added bunny require

--- lib/classroom.rb
+++ lib/classroom.rb
@@ -4,6 +4,8 @@ end
 require 'mumukit/service'
 require 'active_support/all'
 
+require 'bunny'
+
 require_relative './classroom/database'
 require_relative './classroom/json_wrapper'
 require_relative './classroom/follower'
