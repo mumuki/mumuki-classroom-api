3d8b879cc2e5266d89974d79885a7ea3cb8dad20
Federico Scarpa <fedescarpa@gmail.com>
2016-05-04 16:08:00 -0300

    removing course prefix from comments

--- lib/classroom/collection/comments.rb
+++ lib/classroom/collection/comments.rb
@@ -2,6 +2,10 @@ class Classroom::Collection::Comments < Classroom::Collection::CourseCollection
 
   private
 
+  def mongo_collection_name
+    underscore_class_name.to_sym
+  end
+
   def wrap_array(it)
     Classroom::Collection::CommentArray.new(it)
   end
