bb55ce098c146e563c684270ce7e89d2e3997b93
Agustin Pina <aguspina87@gmail.com>
2016-05-17 17:55:49 -0300

    Fix upsert logic

--- lib/classroom/collection/exams.rb
+++ lib/classroom/collection/exams.rb
@@ -5,8 +5,11 @@ class Classroom::Collection::Exams < Classroom::Collection::CourseCollection
 
   def upsert!(data)
     update_one(
-      { 'slug' => data['slug'], 'begin' => data['begin'], 'end' => data['end'], 'duration' => data['duration'] },
-      { '$addToSet' => { 'social_ids' => { '$each' => data['social_ids'] }}},
+      { 'slug' => data['slug']},
+      {
+        '$set' => data.except('social_ids'),
+        '$addToSet' => { 'social_ids' => { '$each' => data['social_ids'] }}
+      },
       { :upsert => true })
   end
 
