2cae1f51e0e16a28b0dd5c33685ac098ecefb6d2
Agustin Pina <aguspina87@gmail.com>
2016-05-28 11:01:49 -0300

    Refactor move student logic to people

---
+++ lib/classroom/collection/people.rb
@@ -0,0 +1,26 @@
+class Classroom::Collection::People < Classroom::Collection::CourseCollection
+
+  def find_by(args)
+    first_by(args, { _id: -1 })
+  end
+
+  def wrap(it)
+    date_wrap super(it)
+  end
+
+  def date_wrap(it)
+    it.raw[:created_at] = it.raw.delete(:_id).generation_time
+    it
+  end
+
+  def find_projection(args={}, projection={})
+    mongo_collection.find(args).projection(projection)
+  end
+
+
+  def ensure_new!(social_id)
+    raise exists_exception, "#{underscore_class_name.capitalize.singularize} already exist" if any?(social_id: social_id)
+  end
+
+end
+
--- lib/classroom/collection/students.rb
+++ lib/classroom/collection/students.rb
@@ -1,25 +1,7 @@
-class Classroom::Collection::Students < Classroom::Collection::CourseCollection
+class Classroom::Collection::Students < Classroom::Collection::People
 
-  def find_by(args)
-    first_by(args, { _id: -1 })
-  end
-
-  def wrap(it)
-    date_wrap super(it)
-  end
-
-  def date_wrap(it)
-    it.raw[:created_at] = it.raw.delete(:_id).generation_time
-    it
-  end
-
-  def find_projection(args={}, projection={})
-    mongo_collection.find(args).projection(projection)
-  end
-
-
-  def ensure_new!(social_id)
-    raise Classroom::StudentExistsError, 'Student already exist' if any?(social_id: social_id)
+  def exists_exception
+    Classroom::StudentExistsError
   end
 
   def update_all_stats
