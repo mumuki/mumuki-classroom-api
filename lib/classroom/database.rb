6a0a4c1a45d371a65d5703a1e2a485bb9920a054
Federico Scarpa <fedescarpa@gmail.com>
2016-06-14 16:52:23 -0300

    open class to define class methods

--- lib/classroom/database.rb
+++ lib/classroom/database.rb
@@ -1,25 +1,27 @@
 class Classroom::Database
-
-  extend Mumukit::Service::Database
-
+  
   class << self
+
+    include Mumukit::Service::Database
+
     attr_reader :organization
-  end
 
-  def self.client
-    @client
-  end
-
-  def self.organization=(organization)
-    @organization = organization
-    @client = new_database_client(organization)
-  end
-
-  def self.within_each(&block)
-    client.database_names.each do |organization|
-      self.organization = organization.to_sym
-      block.call
+    def client
+      @client
     end
+
+    def organization=(organization)
+      @organization = organization
+      @client = new_database_client(organization)
+    end
+
+    def within_each(&block)
+      client.database_names.each do |organization|
+        self.organization = organization.to_sym
+        block.call
+      end
+    end
+
   end
 
 end
