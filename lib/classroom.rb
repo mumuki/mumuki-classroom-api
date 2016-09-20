47b00ffdd720ace19cbae7b7bb2621a2d2b286ef
Agustin Pina <aguspina87@gmail.com>
2016-09-20 18:58:00 -0300

    Reorder atheneum file

--- lib/classroom.rb
+++ lib/classroom.rb
@@ -10,10 +10,10 @@ require 'bunny'
 
 require_relative './classroom/database'
 require_relative './classroom/json_wrapper'
+require_relative './classroom/atheneum'
 require_relative './classroom/collection'
 require_relative './classroom/follower'
 require_relative './classroom/submission'
-require_relative './classroom/atheneum'
 require_relative './classroom/failed_submission'
 require_relative './classroom/comments'
 
