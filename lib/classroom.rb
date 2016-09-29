5ef83e7bb0f4dda158085553f022c56680713a00
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-09-29 13:46:21 -0300

    Splitting code into collections and documents

--- lib/classroom.rb
+++ lib/classroom.rb
@@ -8,8 +8,10 @@ require 'mumukit/inspection'
 require_relative './class'
 require_relative './classroom/database'
 require_relative './classroom/json_wrapper'
-require_relative './classroom/collection'
-require_relative './classroom/follower'
+
+require_relative './classroom/collections'
+require_relative './classroom/documents'
+
 require_relative './classroom/submission'
 require_relative './classroom/failed_submission'
 require_relative './classroom/comments'
