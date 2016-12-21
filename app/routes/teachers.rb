cf9f54bacba3f7dfe43a73d7a8cc47014a48f991
Federico Scarpa <fedescarpa@gmail.com>
2016-12-21 17:03:09 -0300

    removed POST teacher endpoint

--- app/routes/teachers.rb
+++ app/routes/teachers.rb
@@ -2,19 +2,3 @@ get '/courses/:course/teachers' do
   protect! :teacher
   Classroom::Collection::Teachers.for(course).all.as_json
 end
-
-post '/courses/:course/teachers' do
-  protect! :teacher
-
-  Mumukit::Auth::User.from_email(json_body['email']).tap do |user|
-    ensure_course_existence!
-    Classroom::Collection::Teachers.for(course).ensure_new! user.social_id, json_body['email']
-    Classroom::Collection::Teachers.for(course).insert!(json_body.merge(image_url: user.user['picture'], social_id: user.social_id).wrap_json)
-    Classroom::Collection::Students.for(course).delete!(user.social_id)
-
-    user.add_permission!('classroom', course_slug)
-    user.add_permission!('atheneum', "#{tenant}/*")
-  end
-
-  {status: :created}
-end
