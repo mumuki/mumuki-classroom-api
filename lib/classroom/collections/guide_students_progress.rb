bb9510bbb54a5ad18b62c035a2456920f7eb3b44
Federico Scarpa <fedescarpa@gmail.com>
2016-12-23 11:52:22 -0300

    using uid in guide strudent progress

--- lib/classroom/collections/guide_students_progress.rb
+++ lib/classroom/collections/guide_students_progress.rb
@@ -4,8 +4,8 @@ class Classroom::Collection::GuideStudentsProgress < Classroom::Collection::Cour
     mongo_collection.update_one(query_by_index(guide_student), { :'$set' => guide_student }, { upsert: true })
   end
 
-  def last_assignment_for(social_id)
-    guide_student_progress = first_by({'student.social_id' => social_id }, { 'last_assignment.submission.created_at' => -1 })
+  def last_assignment_for(uid)
+    guide_student_progress = first_by({'student.uid': uid }, { 'last_assignment.submission.created_at': -1 })
     guide_student_progress.try do |it|
       {
         guide: it.guide,
@@ -19,39 +19,25 @@ class Classroom::Collection::GuideStudentsProgress < Classroom::Collection::Cour
     end
   end
 
-  def delete_student!(social_id)
-    mongo_collection.delete_many(:'student.social_id' => :social_id)
-  end
-
-  def detach_student!(social_id)
+  def detach_student!(uid)
     mongo_collection.update_many(
-      { :'student.social_id' => social_id },
+      { :'student.uid' => uid },
       { :$set => { detached: true }}
     )
   end
 
-  def attach_student!(social_id)
+  def attach_student!(uid)
     mongo_collection.update_many(
-      { :'student.social_id' => social_id },
+      { :'student.uid' => uid },
       { :$unset => { detached: '' }}
     )
-  end
-
-  def transfer(social_id, destination)
-    where(:'student.social_id' => social_id).raw.each do |guide_progress_data|
-      guide_progress = guide_progress_data.raw.deep_symbolize_keys
-      Classroom::Collection::GuideStudentsProgress.for(destination).insert! guide_progress_data
-      Classroom::Collection::Guides.for(course).transfer(guide_progress[:guide][:slug], destination)
-    end
-    delete_student!(social_id)
-    Classroom::Collection::Guides.for(course).delete_if_has_no_progress
   end
 
   private
 
   def query_by_index(guide_student)
     { :'guide.slug' => guide_student[:guide][:slug],
-      :'student.social_id' => guide_student[:student][:social_id] }
+      :'student.uid' => guide_student[:student][:uid] }
   end
 
 end
