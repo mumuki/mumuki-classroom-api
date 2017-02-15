f7dbe4a098ace440e268b92d50f74ac7c4e9e4d4
Agustin Pina <aguspina87@gmail.com>
2017-02-15 14:09:44 -0300

    Add cokkies domain

--- app/session_store.rb
+++ app/session_store.rb
@@ -1,1 +1,1 @@
-use Rack::Session::Cookie, key: '_mumuki_bibliotheca_session', secret: ENV['SECRET_KEY_BASE']
+use Rack::Session::Cookie, key: '_mumuki_classroom_session', secret: ENV['SECRET_KEY_BASE']
