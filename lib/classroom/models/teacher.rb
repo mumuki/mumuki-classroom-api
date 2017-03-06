f3a27c1d6b870824085b5cbb22d9e722fb7cb4b2
Federico Scarpa <fedescarpa@gmail.com>
2017-03-06 15:15:04 -0300

    added name to teacher

--- lib/classroom/models/teacher.rb
+++ lib/classroom/models/teacher.rb
@@ -4,13 +4,14 @@ class Teacher
   include Mongoid::Timestamps
 
   field :uid, type: String
-  field :email, type: String
-  field :social_id, type: String
   field :first_name, type: String
   field :last_name, type: String
   field :image_url, type: String
-  field :course, type: String
+  field :name, type: String
+  field :email, type: String
+  field :social_id, type: String
   field :organization, type: String
+  field :course, type: Mumukit::Auth::Slug
 
   create_index({organization: 1, course: 1, uid: 1}, {unique: true})
 
