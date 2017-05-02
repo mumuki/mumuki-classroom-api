2eec34a171a330a01ebe1a7d001dfe0020a5937a
Federico Scarpa <fedescarpa@gmail.com>
2017-05-02 17:38:48 -0300

    quick fix loggin in path mapping

--- lib/classroom.rb
+++ lib/classroom.rb
@@ -52,3 +52,17 @@ Mumukit::Platform.configure do |config|
   config.web_framework = Mumukit::Platform::WebFramework::Sinatra
 end
 
+module Mumukit::Platform::OrganizationMapping::Path
+  class << self
+    alias_method :__organization_name__, :organization_name
+
+    def organization_name(request, domain)
+      name = __organization_name__(request, domain)
+      if %w(auth login logout).include? name
+        'central'
+      else
+        name
+      end
+    end
+  end
+end
