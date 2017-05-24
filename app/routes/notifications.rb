39b6ab6696bf80ade1960bd5b12bb199b1e556a9
Agustin Pina <aguspina87@gmail.com>
2017-05-24 12:39:46 -0300

    Remove duplicated routes

--- app/routes/notifications.rb
+++ app/routes/notifications.rb
@@ -1,25 +1,27 @@
-get '/notifications/unread' do
-  authorize! :teacher
-  Notification.unread(organization, current_user.permissions)
-end
+Mumukit::Platform.map_organization_routes!(self) do
+  get '/notifications/unread' do
+    authorize! :teacher
+    Notification.unread(organization, current_user.permissions)
+  end
 
-get '/notifications' do
-  authorize! :teacher
-  page = params[:page].to_i
-  per_page = params[:per_page].to_i
-  {total: Notification.count,
-   page: page,
-   notifications: Notification.page(organization, current_user.permissions, page, per_page)}
-end
+  get '/notifications' do
+    authorize! :teacher
+    page = params[:page].to_i
+    per_page = params[:per_page].to_i
+    {total: Notification.count,
+     page: page,
+     notifications: Notification.page(organization, current_user.permissions, page, per_page)}
+  end
 
-put '/notifications/:notificationId/read' do
-  authorize! :teacher
-  Notification.find(params[:notificationId]).read!
-  {status: :updated}
-end
+  put '/notifications/:notificationId/read' do
+    authorize! :teacher
+    Notification.find(params[:notificationId]).read!
+    {status: :updated}
+  end
 
-put '/notifications/:notificationId/unread' do
-  authorize! :teacher
-  Notification.find(params[:notificationId]).unread!
-  {status: :updated}
+  put '/notifications/:notificationId/unread' do
+    authorize! :teacher
+    Notification.find(params[:notificationId]).unread!
+    {status: :updated}
+  end
 end
