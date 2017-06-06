8545a92620da44afe3c25fd65af4698bee719051
Federico Scarpa <fedescarpa@gmail.com>
2017-06-06 12:59:48 -0300

    retiving email in followers from current_user

--- app/routes/followers.rb
+++ app/routes/followers.rb
@@ -1,36 +1,25 @@
 helpers do
   def follower_query
-    with_organization_and_course email: params[:email]
-  end
-
-  def follower_query_from_body
-    with_organization_and_course email: json_body[:email]
+    with_organization_and_course email: current_user.uid
   end
 end
 
 Mumukit::Platform.map_organization_routes!(self) do
   post '/courses/:course/followers' do
     authorize! :teacher
-    Follower.find_or_create_by!(follower_query_from_body).add!(json_body[:uid])
-    {status: :created}
-  end
-
-  post '/courses/:course/followers/:email' do
-    authorize! :teacher
     Follower.find_or_create_by!(follower_query).add!(json_body[:uid])
     {status: :created}
   end
 
-  get '/courses/:course/followers/:email' do
+  get '/courses/:course/followers' do
     {followers: Follower
                   .where(follower_query)
-                  .where(email: params[:email])
                   .select { |it| permissions.has_permission? :teacher, it.course }
                   .as_json
     }
   end
 
-  delete '/courses/:course/followers/:email/:uid' do
+  delete '/courses/:course/followers/:uid' do
     authorize! :teacher
     Follower.find_by!(follower_query).remove!(params[:uid])
     {status: :created}
