52e918723dcec0e46ea2d11b783e78f1fc536e90
Federico Scarpa <fedescarpa@gmail.com>
2017-06-08 16:38:03 -0300

    extract message logic

--- app/routes/messages.rb
+++ app/routes/messages.rb
@@ -1,13 +1,20 @@
+helpers do
+  def message
+    message = json_body[:message]
+    message[:sender] = current_user.uid
+    message
+  end
+end
+
 Mumukit::Platform.map_organization_routes!(self) do
   post '/courses/:course/messages' do
     authorize! :teacher
-    message = json_body[:message]
-    message[:sender] = current_user.uid
     assignment = Assignment.find_by!(with_organization_and_course 'exercise.eid': json_body[:exercise_id], 'student.uid': json_body[:uid])
     assignment.add_message! message, json_body[:submission_id]
     assignment.notify_message! message, json_body[:submission_id]
     {status: :created, message: Message.new(message)}
   end
+
   get '/courses/:course/guides/:organization/:repository/:uid/:exercise_id/messages' do
     authorize! :student
     threads = Assignment.find_by!(with_organization_and_course exercise_student_progress_query.merge('exercise.eid': exercise_id)).threads(params[:language])
