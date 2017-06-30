29985d208de745276441e0c7b2fd4206bc7df4de
Federico Aloi <federico.aloi@gmail.com>
2017-06-30 19:56:38 -0300

    Suggestion.times_used is persisted

--- lib/classroom/models/suggestion.rb
+++ lib/classroom/models/suggestion.rb
@@ -8,15 +8,14 @@ class Suggestion
   field :content, type: String
   field :date, type: String
   field :guide_slug, type: String
+  field :times_used, type: Integer
+
+  before_save :update_times_used
 
   embeds_one :exercise
   embeds_many :submissions
 
   create_index({'guide_slug': 1, 'exercise.eid': 1})
-
-  def times_used
-    self.submissions.count
-  end
 
   def add_submission!(submission)
     self.submissions << submission
@@ -25,5 +24,11 @@ class Suggestion
 
   def self.create_from(message, assignment)
     self.create message.merge(guide_slug: assignment.guide['slug'], exercise: assignment.exercise)
+  end
+
+  private
+
+  def update_times_used
+    self.times_used = self.submissions.size
   end
 end
