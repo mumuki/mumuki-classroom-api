eebacf9ddd1922cc97bcfa00decf509fed8e7ee9
Federico Scarpa <fedescarpa@gmail.com>
2017-08-28 18:54:27 -0300

    invitation model

---
+++ lib/classroom/models/invitation.rb
@@ -0,0 +1,26 @@
+class Invitation
+
+  include Mongoid::Document
+
+  field :code, type: String, default: -> { Invitation.generate_code }
+  field :course_slug, type: String
+  field :expiration_date, type: Time
+  belongs_to :course
+
+  before_validation :check_if_expired!
+
+  def check_if_expired!
+    if expired?
+      raise Exception.new "Must be in future"
+    end
+  end
+
+  def expired?
+    Time.now > expiration_date
+  end
+
+  def self.generate_code
+    SecureRandom.urlsafe_base64 4
+  end
+
+end
