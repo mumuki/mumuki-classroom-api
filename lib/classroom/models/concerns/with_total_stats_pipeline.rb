356626f2d0ef9cf5b311681d641287122f3bd71d
Franco Bulgarelli <franco@mumuki.org>
2017-12-24 17:03:07 -0300

    Using less clumsy indentation

--- lib/classroom/models/concerns/with_total_stats_pipeline.rb
+++ lib/classroom/models/concerns/with_total_stats_pipeline.rb
@@ -1,13 +1,5 @@
 module WithTotalStatsPipeline
   def pipeline
-    [
-      {
-        '$addFields': {
-          'stats.total': {
-            '$sum': %w($stats.passed $stats.passed_with_warnings $stats.failed)
-          }
-        }
-      }
-    ]
+    [{ '$addFields': { 'stats.total': { '$sum': %w($stats.passed $stats.passed_with_warnings $stats.failed) } } }]
   end
 end
