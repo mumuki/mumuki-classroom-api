8a87e732469044e07e5a1270e3b16ce472ea671e
Franco Bulgarelli <franco@mumuki.org>
2018-03-29 11:47:40 -0300

    Adding docs

--- lib/events.rb
+++ lib/events.rb
@@ -1,14 +1,19 @@
 Mumukit::Nuntius::EventConsumer.handle do
 
-
+  # Emitted by:
+  #    * new logins in laboratory
+  #    * user creation and modification in laboratory
+  #    * user creation and modification in classroom
   event 'UserChanged' do |payload|
     Classroom::Event::UserChanged.execute! payload[:user].except(:created_at, :updated_at)
   end
 
+  # Emitted by course creation and modification in laboratory
   event 'CourseChanged' do |payload|
     Course.import_from_json! payload[:course].except(:created_at, :updated_at)
   end
 
+  # Emitted by organization creation and modification in laboratory
   event 'OrganizationChanged' do |payload|
     Organization.import_from_json! payload[:organization].except(:created_at, :updated_at)
   end
