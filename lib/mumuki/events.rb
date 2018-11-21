b235241c3bc722bc6558875b890d9918994dc4ae
Franco Bulgarelli <franco@mumuki.org>
2018-11-21 20:45:36 -0300

    Updating event consumers

--- lib/mumuki/events.rb
+++ lib/mumuki/events.rb
@@ -5,16 +5,16 @@ Mumukit::Nuntius::EventConsumer.handle do
   #    * user creation and modification in laboratory
   #    * user creation and modification in classroom
   event 'UserChanged' do |payload|
-    Mumuki::Classroom::Event::UserChanged.execute! payload[:user].except(:created_at, :updated_at)
+    Mumuki::Classroom::Event::UserChanged.execute! payload.deep_symbolize_keys[:user].except(:created_at, :updated_at)
   end
 
   # Emitted by course creation and modification in laboratory
   event 'CourseChanged' do |payload|
-    Course.import_from_json! payload[:course].except(:created_at, :updated_at)
+    Course.import_from_resource_h! payload.deep_symbolize_keys[:course]
   end
 
   # Emitted by organization creation and modification in laboratory
   event 'OrganizationChanged' do |payload|
-    Organization.import_from_json! payload[:organization].except(:created_at, :updated_at)
+    Organization.import_from_resource_h! payload.deep_symbolize_keys[:organization]
   end
 end
