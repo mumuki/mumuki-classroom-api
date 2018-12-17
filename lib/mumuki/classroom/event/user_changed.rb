0f8712b46f4bb9e9348750c94d81f2bb00ca7fbf
Franco Bulgarelli <franco@mumuki.org>
2018-12-17 18:12:58 -0300

    Renaming some methods

--- lib/mumuki/classroom/event/user_changed.rb
+++ lib/mumuki/classroom/event/user_changed.rb
@@ -6,15 +6,15 @@ class Mumuki::Classroom::Event::UserChanged
     def execute!(user_h)
       user = Mumukit::Platform::User::Helpers.slice_platform_json user_h.compact
       set_diff_permissions user
-      update_user_model user.except(:permissions)
+      update_user_roles! user.except(:permissions)
       User.import_from_resource_h! user # FIXME we must refactor events
     end
 
     private
 
-    def update_user_model(user)
+    def update_user_roles!(user)
       Organization.pluck(:name).each do |organization|
-        update_student organization, user
+        update_student! organization, user
         changes[organization]&.each do |change|
           message = change.description
           self.send message, organization, user, change.granted_slug if self.respond_to? message, true
@@ -27,16 +27,15 @@ class Mumuki::Classroom::Event::UserChanged
       self.changes = Mumukit::Auth::Permissions::Diff.diff(permissions, user[:permissions]).changes_by_organization
     end
 
-    def update_student(organization, user)
+    def update_student!(organization, user)
       Mumuki::Classroom::Student.last_updated_student_by(organization: organization, uid: user[:uid]).try do |student|
         old_profile = Mumukit::Auth::Profile.extract student
         new_profile = Mumukit::Auth::Profile.extract user
-        update_student! organization, student.course, new_profile.attributes if old_profile != new_profile
+        update_student_embeddings! organization, student.course, new_profile.attributes if old_profile != new_profile
       end
     end
 
-
-    def update_student!(organization, course_slug, student_h)
+    def update_student_embeddings!(organization, course_slug, student_h)
       sub_student = student_h.transform_keys { |field| "student.#{field}".to_sym }
       Mumuki::Classroom::Student.find_by!(organization: organization, course: course_slug, uid: student_h[:uid]).update_attributes! student_h
       Mumuki::Classroom::GuideProgress.where(organization: organization, course: course_slug, 'student.uid': student_h[:uid]).update_all sub_student
