d1eb52376c68ea10b6e7ef2410adbfd58a761188
Julián Berbel Alt <julianberbel@hotmail.com>
2019-03-22 18:11:52 -0300

    add report test

--- spec/students_spec.rb
+++ spec/students_spec.rb
@@ -460,5 +460,60 @@ describe Student do
     end
   end
 
+  describe 'get students/report' do
+    let(:student) { {
+      organization: 'example.org',
+      course: 'example.org/foo',
+      uid: 'foo@bar.com',
+      created_at: '2016-08-01T18:39:57.000Z',
+      email: 'foo@bar.com',
+      personal_id: '1234',
+      first_name: 'Foo',
+      last_name: 'Bar',
+      last_assignment: {
+        exercise: {
+          eid: 1,
+          name: 'Test',
+          number: 1,
+        },
+        guide: {
+          name: 'Exam Test',
+          slug: 'foo/bar',
+          language: {
+            name: 'javascript'
+          },
+          parent: {
+            type: 'Exam',
+            name: 'Exam Test'
+          }
+        },
+        submission: {
+          created_at: '2016-08-01T18:39:57.481Z',
+          sid: '6a6ea7df6e55fbba',
+          status: 'failed'
+        }
+      },
+      stats: {
+        failed: 27,
+        passed: 117,
+        passed_with_warnings: 1
+      }
+    } }
+    before { Student.create! student }
+    before { Student.create! student.merge uid: 'bar@baz.com', email: 'bar@baz.com', personal_id: '9191', stats: {failed: 27, passed: 100, passed_with_warnings: 2} }
+    before { Student.create! student.merge uid: 'baz@bar.com', email: 'baz@bar.com', personal_id: '1212', stats: {failed: 27, passed: 120, passed_with_warnings: 2}, course: 'example.org/bar' }
+    before { Student.create! student.merge first_name: 'Bar', uid: 'bar@foo.com', email: 'bar@foo.com', personal_id: '2222', stats: {failed: 27, passed: 120, passed_with_warnings: 1}, course: 'example.org/bar' }
+    before { header 'Authorization', build_auth_header('*') }
+    before { get '/students/report' }
+    it do
+      expect(last_response.body).to eq <<TEST
+last_name,first_name,email,personal_id,created_at,last_submission_date,passed_count,passed_with_warnings_count,failed_count,last_lesson_type,last_lesson_name,last_exercise_number,last_exercise_name,last_chapter,course
+Bar,Foo,baz@bar.com,1212,2016-08-01T18:39:57.000Z,2016-08-01T18:39:57.481Z,120,2,27,Exam,Exam Test,1,Test,example.org/bar
+Bar,Bar,bar@foo.com,2222,2016-08-01T18:39:57.000Z,2016-08-01T18:39:57.481Z,120,1,27,Exam,Exam Test,1,Test,example.org/bar
+Bar,Foo,foo@bar.com,1234,2016-08-01T18:39:57.000Z,2016-08-01T18:39:57.481Z,117,1,27,Exam,Exam Test,1,Test,example.org/foo
+Bar,Foo,bar@baz.com,9191,2016-08-01T18:39:57.000Z,2016-08-01T18:39:57.481Z,100,2,27,Exam,Exam Test,1,Test,example.org/foo
+TEST
+    end
+  end
 
 end
