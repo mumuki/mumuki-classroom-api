9f8927bfad46e590375fc4033bcbc25ea5d86caa
Lucho Cannavo <luchotc@hotmail.com.ar>
2019-10-15 12:32:43 -0300

    Adding test for expected result

--- spec/courses_spec.rb
+++ spec/courses_spec.rb
@@ -133,7 +133,7 @@ describe Course do
   end
 
   describe 'get courses/:course/report' do
-    let(:student) { {
+    let(:full_student) { {
       organization: 'example.org',
       course: 'example.org/foo',
       uid: 'foo@bar.com',
@@ -171,20 +171,37 @@ describe Course do
         passed_with_warnings: 1
       }
     } }
+    let(:student) { full_student }
     before { Student.create! student }
     before { Student.create! student.merge uid: 'bar@baz.com', email: 'bar@baz.com', personal_id: '9191', stats: {failed: 27, passed: 100, passed_with_warnings: 2} }
     before { Student.create! student.merge uid: 'baz@bar.com', email: 'baz@bar.com', personal_id: '1212', stats: {failed: 27, passed: 120, passed_with_warnings: 2} }
     before { Student.create! student.merge first_name: 'Bar', uid: 'bar@foo.com', email: 'bar@foo.com', personal_id: '2222', stats: {failed: 27, passed: 120, passed_with_warnings: 1} }
     before { header 'Authorization', build_auth_header('*') }
     before { get '/courses/foo/report' }
-    it do
-      expect(last_response.body).to eq <<TEST
+
+    context 'when fields are complete' do
+      it do
+        expect(last_response.body).to eq <<TEST
 last_name,first_name,email,personal_id,detached,created_at,last_submission_date,passed_count,passed_with_warnings_count,failed_count,last_lesson_type,last_lesson_name,last_exercise_number,last_exercise_name,last_chapter
 Bar,Foo,baz@bar.com,1212,false,2016-08-01T18:39:57.000Z,2016-08-01T18:39:57.481Z,120,2,27,Exam,Exam Test,1,Test
 Bar,Bar,bar@foo.com,2222,false,2016-08-01T18:39:57.000Z,2016-08-01T18:39:57.481Z,120,1,27,Exam,Exam Test,1,Test
 Bar,Foo,foo@bar.com,1234,false,2016-08-01T18:39:57.000Z,2016-08-01T18:39:57.481Z,117,1,27,Exam,Exam Test,1,Test
 Bar,Foo,bar@baz.com,9191,false,2016-08-01T18:39:57.000Z,2016-08-01T18:39:57.481Z,100,2,27,Exam,Exam Test,1,Test
 TEST
+      end
+    end
+
+    context 'when fields aren\'t complete' do
+      let(:student) { full_student.except(:first_name, :personal_id) }
+      it do
+        expect(last_response.body).to eq <<TEST
+last_name,first_name,email,personal_id,detached,created_at,last_submission_date,passed_count,passed_with_warnings_count,failed_count,last_lesson_type,last_lesson_name,last_exercise_number,last_exercise_name,last_chapter
+Bar,,baz@bar.com,,false,2016-08-01T18:39:57.000Z,2016-08-01T18:39:57.481Z,120,2,27,Exam,Exam Test,1,Test
+Bar,,bar@foo.com,,false,2016-08-01T18:39:57.000Z,2016-08-01T18:39:57.481Z,120,1,27,Exam,Exam Test,1,Test
+Bar,,foo@bar.com,,false,2016-08-01T18:39:57.000Z,2016-08-01T18:39:57.481Z,117,1,27,Exam,Exam Test,1,Test
+Bar,,bar@baz.com,,false,2016-08-01T18:39:57.000Z,2016-08-01T18:39:57.481Z,100,2,27,Exam,Exam Test,1,Test
+TEST
+      end
     end
   end
 
