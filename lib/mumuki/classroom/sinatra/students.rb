984d4d2e1dd40109687acc5db8395af79ef1bbe8
Lucho Cannavo <luchotc@hotmail.com.ar>
2019-10-30 20:05:26 -0300

    Adding comments for students endpoints

--- lib/mumuki/classroom/sinatra/students.rb
+++ lib/mumuki/classroom/sinatra/students.rb
@@ -37,17 +37,20 @@ Mumukit::Platform.map_organization_routes!(self) do
     group_report with_organization, group_report_projection.merge(course: '$course')
   end
 
+  # Retrieves the progress for a student in an specific course
   get '/api/courses/:course/students/:uid' do
     authorize! :teacher
     {guide_students_progress: Mumuki::Classroom::GuideProgress.where(with_organization_and_course 'student.uid': uid).sort(created_at: :asc).as_json}
   end
 
+  # Tries to resubmit all failed_submissions of a student to a specific tenant
   post '/courses/:course/students/:uid' do
     authorize! :janitor
     Mumukit::Nuntius.notify! 'resubmissions', uid: uid, tenant: tenant
     {status: :created}
   end
 
+  # Detaches a student of a course
   post '/courses/:course/students/:uid/detach' do
     authorize! :janitor
     Mumuki::Classroom::Student.find_by!(with_organization_and_course uid: uid).detach!
@@ -55,6 +58,7 @@ Mumukit::Platform.map_organization_routes!(self) do
     {status: :updated}
   end
 
+  # Attaches a student to a course
   post '/courses/:course/students/:uid/attach' do
     authorize! :janitor
     Mumuki::Classroom::Student.find_by!(with_organization_and_course uid: uid).attach!
@@ -62,6 +66,7 @@ Mumukit::Platform.map_organization_routes!(self) do
     {status: :updated}
   end
 
+  # Transfers a student to another course
   post '/courses/:course/students/:uid/transfer' do
     authorize! :janitor
 
@@ -75,19 +80,20 @@ Mumukit::Platform.map_organization_routes!(self) do
     {status: :updated}
   end
 
+  # Retrieves info for a particular student
   get '/courses/:course/student/:uid' do
     authorize! :teacher
 
     Mumuki::Classroom::Student.find_by!(with_organization_and_course uid: uid).as_json
   end
 
+  # Creates student and tries to resubmit all failed submissions to that student
   post '/courses/:course/students' do
     authorize! :janitor
     ensure_course_existence!
     ensure_student_not_exists!
 
     normalize_student!
-
     json = {student: json_body.merge(uid: json_body[:email]), course: {slug: course_slug}}
     uid = json[:student][:uid]
 
@@ -103,6 +109,7 @@ Mumukit::Platform.map_organization_routes!(self) do
     {status: :created}
   end
 
+  # Updates student information
   put '/courses/:course/students/:uid' do
     authorize! :janitor
     ensure_course_existence!
