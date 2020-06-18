1ef298575c1b689b5cb2cafcf14c61eb1b5ecf42
Federico Scarpa <fedescarpa@gmail.com>
2020-06-18 11:46:52 -0300

    Remove all notifications of CourseChanged event

--- lib/mumuki/events.rb
+++ lib/mumuki/events.rb
@@ -7,9 +7,4 @@ Mumukit::Nuntius::EventConsumer.handle do
   event 'UserChanged' do |payload|
     Mumuki::Classroom::Event::UserChanged.execute! payload.deep_symbolize_keys[:user].except(:created_at, :updated_at)
   end
-
-  # Emitted by course creation and modification in laboratory
-  event 'CourseChanged' do |payload|
-    # Course.import_from_resource_h! payload.deep_symbolize_keys[:course]
-  end
 end
