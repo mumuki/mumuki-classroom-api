0d2e7d84cbf157dbf874624b46ba2a82e4363a52
Julián Berbel Alt <julian@mumuki.org>
2020-11-26 17:18:08 -0300

    Set created_at and updated_at only if not previously set on insert_many

--- lib/mumuki/classroom/collection.rb
+++ lib/mumuki/classroom/collection.rb
@@ -2,8 +2,8 @@ class Mongo::Collection
   revamp :insert_many do |_, _, documents, *args, hyper|
     now = Time.now
     documents.map do |it|
-      it[:created_at] = now
-      it[:updated_at] = now
+      it[:created_at] ||= now
+      it[:updated_at] ||= now
     end
     hyper.call documents, *args
   end
