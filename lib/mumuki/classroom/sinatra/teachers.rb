b8806ee778563df0f107593b0b17454d84fbf384
Franco Leonardo Bulgarelli <franco@mumuki.org>
2021-02-26 11:55:54 -0300

    Generalize teachers list

--- lib/mumuki/classroom/sinatra/teachers.rb
+++ lib/mumuki/classroom/sinatra/teachers.rb
@@ -1,8 +1,18 @@
 class Mumuki::Classroom::App < Sinatra::Application
   Mumukit::Platform.map_organization_routes!(self) do
+    helpers do
+      def list_teachers(matcher)
+        authorize! :headmaster
+        {teachers: Mumuki::Classroom::Teacher.where(matcher).as_json}
+      end
+    end
+
+    get '/teachers' do
+      list_teachers with_organization
+    end
+
     get '/courses/:course/teachers' do
-      authorize! :headmaster
-      {teachers: Mumuki::Classroom::Teacher.where(with_organization_and_course).as_json}
+      list_teachers with_organization_and_course
     end
 
     post '/courses/:course/teachers' do
