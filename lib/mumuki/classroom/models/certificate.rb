8d416dffe09bbbc11c79721d7f59aaabed2b7c51
Julián Berbel Alt <julian@mumuki.org>
2021-11-11 16:43:38 -0300

    Add missing fields and indexes

--- lib/mumuki/classroom/models/certificate.rb
+++ lib/mumuki/classroom/models/certificate.rb
@@ -3,8 +3,13 @@ class Mumuki::Classroom::Certificate < Mumuki::Classroom::Document
 
   field :pg_id, type: Integer
   field :code, type: String
-  field :certificate_program, type: Hash
+  field :certificate_program_id, type: Integer
+  field :organization, type: String
   field :student, type: Hash
   field :started_at, type: Time
   field :ended_at, type: Time
+
+  create_index(pg_id: 1)
+  create_index(organization: 1, 'student.course': 1, 'student.uid': 1)
+  create_index(certificate_program_id: 1)
 end
--- lib/mumuki/classroom/models/exam_authorization_request.rb
+++ lib/mumuki/classroom/models/exam_authorization_request.rb
@@ -2,8 +2,14 @@ class Mumuki::Classroom::ExamAuthorizationRequest < Mumuki::Classroom::Document
   include Mongoid::Timestamps
 
   field :pg_id, type: Integer
-  field :status, type: Integer
+  field :status, type: String
   field :guide_slug, type: String
-  field :exam_registration, type: Hash
+  field :organization, type: String
+  field :exam_registration_id, type: Integer
   field :student, type: Hash
+
+  create_index(pg_id: 1)
+  create_index(organization: 1, 'student.course': 1, guide_slug: 1, 'student.uid': 1)
+  create_index(exam_registration_id: 1)
+  create_index(organization: 1, 'student.course': 1, 'student.uid': 1)
 end
